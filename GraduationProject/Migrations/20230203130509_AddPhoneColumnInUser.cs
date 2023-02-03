1880f9f984299fe4148582e3dfeb5b3ba63b2c01
omniashehata36@gmail.com <omniashehata36@gmail.com>
2023-02-03 15:08:39 +0200

    -

---
+++ GraduationProject/Migrations/20230203130509_AddPhoneColumnInUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GraduationProject.Migrations
+{
+    public partial class AddPhoneColumnInUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Users");
+        }
+    }
+}
