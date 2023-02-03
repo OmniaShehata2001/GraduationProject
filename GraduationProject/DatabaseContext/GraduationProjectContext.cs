ca1a2e0f2e2fcd4c71725d0a08ad71ec42c9d3a8
ahmed <ahmed@LAPTOP-J2L6Q06P>
2023-02-03 14:18:02 +0200

    Add project files.

---
+++ GraduationProject/DatabaseContext/GraduationProjectContext.cs
@@ -0,0 +1,22 @@
+﻿using GraduationProject.GraduationProjectLogic;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraduationProject.DatabaseContext
+{
+    public class GraduationProjectContext :  DbContext
+    {
+
+
+        public GraduationProjectContext(DbContextOptions<GraduationProjectContext> options) :base(options)
+        {
+
+        }
+
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
